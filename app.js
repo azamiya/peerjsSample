30b9fc300939d9f365a07799cf485dc86999f456
adaniya <t.adaniya@adawarp.com>
2016-04-13 21:13:51 +0900

    add client/datachat.html, js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express'),
     app = express();
 
+//not need from express4.0
 //app.use(app.router);
+app.use(express.static('client'));
 
 app.get('/', function(req, res) {
-    res.send('hello world');
+    res.sendFile(__dirname + "/client/datachat.html")
+    //res.send('hello world');
 });
 app.get('/about', function(req, res) {
     res.send('about this page!');
